c7856ec983f4d32c66720f2173ba0d9882f0c407
dcmunhoz <dc.munhoz@hotmail.com>
2022-02-08 22:24:38 -0300

    Adicionando estilo & realizando bind parâmetros

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
     templateUrl: './nova-transferencia.component.html',
     styleUrls: [ './nova-transferencia.component.scss' ]
 })
-export class NovaTransferenciaComponent { }
+export class NovaTransferenciaComponent 
+{ 
+    public valor: number = 0;
+    public destino: string = "";
+
+
+
+    transferir() {
+        console.log(`Solicitado nova transferência \n\rValor: ${this.valor} \n\rDestino: ${this.destino}`);             
+    }
+
+}
